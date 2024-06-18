dd01da78487b93b1318c061a01914d2a1315519c
baaguimaraes <guimaraes_barbara@sicredi.com.br>
2024-06-17 23:16:51 -0300

    origin main

--- src/BoletimEstudantil.java
+++ src/BoletimEstudantil.java
@@ -11,14 +11,3 @@ public class BoletimEstudantil {
     } 
 }
 
-// COM IDENTAÇÃO
-
-public static void main (String [] args) {
-    int mediaFinal = 6;
-    if(mediaFinal<6)
-       System.out.println("REPROVADO");
-    else if (mediaFinal==6) 
-       System.out.println("PROVA MINERVA");
-    else
-       System.out.println("APROVADO");
-}
